361601bce2605a56ebfd4cdd41c89c16b4e2bc15
Viktor Holovin <holovin@mail.com>
2023-01-21 11:36:36 +0100

    Start spring project

---
+++ src/main/java/ninja/curriculum/portfoliospring/PortfoliospringApplication.java
@@ -0,0 +1,13 @@
+package ninja.curriculum.portfoliospring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PortfoliospringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PortfoliospringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ninja/curriculum/portfoliospring/PortfoliospringApplicationTests.java
@@ -0,0 +1,13 @@
+package ninja.curriculum.portfoliospring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PortfoliospringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
