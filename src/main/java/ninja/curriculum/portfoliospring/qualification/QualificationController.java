b5cf056729c836af91df3eeb68ef653e485fb853
Viktor Holovin <holovin@mail.com>
2023-02-04 15:48:42 +0100

    Add qualification update method

--- src/main/java/ninja/curriculum/portfoliospring/qualification/QualificationController.java
+++ src/main/java/ninja/curriculum/portfoliospring/qualification/QualificationController.java
@@ -3,6 +3,9 @@ package ninja.curriculum.portfoliospring.qualification;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import java.time.LocalDate;
+import java.util.UUID;
+
 @RestController
 @RequestMapping(path = "api/v1/qualification")
 public class QualificationController {
@@ -16,5 +19,10 @@ public class QualificationController {
     @PostMapping
     public void addQualification(@RequestBody Qualification qualification) {
         qualificationService.addQualification(qualification);
+    }
+
+    @PutMapping(path = "{qualificationId}")
+    public void updateQualification(@PathVariable Long qualificationId, @RequestParam(required = false) UUID customerId, @RequestParam(required = false) Long educationalInstitutionId, @RequestParam(required = false) AcademicDegree academicDegree, @RequestParam(required = false) String faculty, @RequestParam(required = false) String department, @RequestParam(required = false) String speciality, LocalDate finishedStudying) {
+        this.qualificationService.updateQualification(qualificationId, customerId, educationalInstitutionId, academicDegree, faculty, department, finishedStudying);
     }
 }
--- src/main/java/ninja/curriculum/portfoliospring/qualification/QualificationService.java
+++ src/main/java/ninja/curriculum/portfoliospring/qualification/QualificationService.java
@@ -1,22 +1,65 @@
 package ninja.curriculum.portfoliospring.qualification;
 
+import ninja.curriculum.portfoliospring.customer.Customer;
+import ninja.curriculum.portfoliospring.customer.CustomerRepository;
+import ninja.curriculum.portfoliospring.educationalinstitution.EducationalInstitution;
+import ninja.curriculum.portfoliospring.educationalinstitution.EducationalInstitutionRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.time.LocalDate;
+import java.util.Optional;
+import java.util.UUID;
 
 
 @Service
 @Transactional(readOnly = true)
 public class QualificationService {
     private final QualificationRepository qualificationRepository;
+    private final CustomerRepository customerRepository;
+    private final EducationalInstitutionRepository educationalInstitutionRepository;
 
     @Autowired
-    public QualificationService(QualificationRepository qualificationRepository) {
+    public QualificationService(QualificationRepository qualificationRepository, CustomerRepository customerRepository, EducationalInstitutionRepository educationalInstitutionRepository) {
         this.qualificationRepository = qualificationRepository;
+        this.customerRepository = customerRepository;
+        this.educationalInstitutionRepository = educationalInstitutionRepository;
     }
 
     @Transactional
     public void addQualification(Qualification qualification) {
         this.qualificationRepository.save(qualification);
     }
-}
+
+    @Transactional
+    public void updateQualification(Long qualificationId, UUID customerId, Long educationalInstitutionId, AcademicDegree academicDegree, String faculty, String department, LocalDate finishedStudying) {
+        Optional<Qualification> qualificationOptional = this.qualificationRepository.findById(qualificationId);
+        if (qualificationOptional.isPresent()) {
+            Qualification qualification = qualificationOptional.get();
+            if (customerId != null) {
+                Optional<Customer> customerOptional = this.customerRepository.findById(customerId);
+                if (customerOptional.isPresent()) {
+                    qualification.setCustomer(customerOptional.get());
+                    this.qualificationRepository.save(qualification);
+                }
+            } else if (educationalInstitutionId != null) {
+                Optional<EducationalInstitution> educationalInstitutionOptional = this.educationalInstitutionRepository.findById(educationalInstitutionId);
+                if (educationalInstitutionOptional.isPresent()) {
+                    qualification.setEducationalInstitution(educationalInstitutionOptional.get());
+                    this.qualificationRepository.save(qualification);
+                }
+            } else if (academicDegree != null) {
+                qualification.setAcademicDegree(academicDegree);
+            } else if (faculty != null) {
+                qualification.setFaculty(faculty);
+            } else if (department != null) {
+                qualification.setDepartment(department);
+            } else if (finishedStudying != null) {
+                qualification.setFinishedStudying(finishedStudying);
+            }
+        } else {
+                throw new IllegalStateException("Argument error");
+            }
+        }
+    }
