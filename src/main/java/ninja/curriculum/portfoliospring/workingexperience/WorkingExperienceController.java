7ce5aa29ba8f21c926592c308fd15b9a38ceae08
Viktor Holovin <holovin@mail.com>
2025-01-03 08:31:42 +0100

    Add attribute DELETE working experience

--- src/main/java/ninja/curriculum/portfoliospring/workingexperience/WorkingExperienceController.java
+++ src/main/java/ninja/curriculum/portfoliospring/workingexperience/WorkingExperienceController.java
@@ -1,6 +1,5 @@
 package ninja.curriculum.portfoliospring.workingexperience;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDate;
@@ -9,31 +8,35 @@ import java.util.List;
 @RestController
 @RequestMapping(path = "/api/v1/working-experience")
 public class WorkingExperienceController {
-    private final WorkingExperienceService workingExperienceService;
+	private final WorkingExperienceService workingExperienceService;
 
-    @Autowired
-    public WorkingExperienceController(WorkingExperienceService workingExperienceService) {
-        this.workingExperienceService = workingExperienceService;
-    }
+	public WorkingExperienceController(WorkingExperienceService workingExperienceService) {
+		this.workingExperienceService = workingExperienceService;
+	}
 
-    @GetMapping(path = "/all")
-    public List<WorkingExperience> getWorkingExperiences() {
-        return this.workingExperienceService.getWorkingExperiences();
-    }
+	@GetMapping(path = "/all")
+	public List<WorkingExperience> getWorkingExperiences() {
+		return this.workingExperienceService.getWorkingExperiences();
+	}
 
-    @PostMapping
-    public void addWorkingExperience(@RequestBody WorkingExperience workingExperience) {
-        this.workingExperienceService.addWorkingExperience(workingExperience);
-    }
+	@PostMapping
+	public void addWorkingExperience(@RequestBody WorkingExperience workingExperience) {
+		this.workingExperienceService.addWorkingExperience(workingExperience);
+	}
 
-    @PutMapping(path = "/{workingExperienceId}/update")
-    public void updateWorkingExperience(@PathVariable Long workingExperienceId,
-                                        @RequestParam(required = false) LocalDate startedDate,
-                                        @RequestParam(required = false) LocalDate finishedDate,
-                                        @RequestParam(required = false) String jobDescription,
-                                        @RequestParam(required = false) String jobDescriptionIt,
-                                        @RequestParam(required = false) Long positionAtWorkId
-    ) {
-        this.workingExperienceService.updateWorkingExperience(workingExperienceId, startedDate, finishedDate, jobDescription, jobDescriptionIt, positionAtWorkId);
-    }
+	@PutMapping(path = "/{workingExperienceId}/update")
+	public void updateWorkingExperience(@PathVariable Long workingExperienceId,
+			@RequestParam(required = false) LocalDate startedDate,
+			@RequestParam(required = false) LocalDate finishedDate,
+			@RequestParam(required = false) String jobDescription,
+			@RequestParam(required = false) String jobDescriptionIt,
+			@RequestParam(required = false) Long positionAtWorkId) {
+		this.workingExperienceService.updateWorkingExperience(workingExperienceId, startedDate, finishedDate,
+				jobDescription, jobDescriptionIt, positionAtWorkId);
+	}
+
+	@DeleteMapping(path = "/delete/{workingExperienceId}")
+	public void deleteWorkingExperience(@PathVariable Long workingExperienceId) {
+		this.workingExperienceService.deleteWorkingExperience(workingExperienceId);
+	}
 }
--- src/main/java/ninja/curriculum/portfoliospring/workingexperience/WorkingExperienceService.java
+++ src/main/java/ninja/curriculum/portfoliospring/workingexperience/WorkingExperienceService.java
@@ -2,7 +2,6 @@ package ninja.curriculum.portfoliospring.workingexperience;
 
 import ninja.curriculum.portfoliospring.company.positionatwork.PositionAtWork;
 import ninja.curriculum.portfoliospring.company.positionatwork.PositionAtWorkRepository;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -10,56 +9,64 @@ import java.time.LocalDate;
 import java.util.List;
 import java.util.Optional;
 
-
 @Service
 @Transactional(readOnly = true)
 public class WorkingExperienceService {
-    private final WorkingExperienceRepository workingExperienceRepository;
-    private final PositionAtWorkRepository positionAtWorkRepository;
+	private final WorkingExperienceRepository workingExperienceRepository;
+	private final PositionAtWorkRepository positionAtWorkRepository;
 
-    @Autowired
-    public WorkingExperienceService(WorkingExperienceRepository workingExperienceRepository, PositionAtWorkRepository positionAtWorkRepository) {
-        this.workingExperienceRepository = workingExperienceRepository;
-        this.positionAtWorkRepository = positionAtWorkRepository;
-    }
+	public WorkingExperienceService(WorkingExperienceRepository workingExperienceRepository,
+			PositionAtWorkRepository positionAtWorkRepository) {
+		this.workingExperienceRepository = workingExperienceRepository;
+		this.positionAtWorkRepository = positionAtWorkRepository;
+	}
 
-    @Transactional
-    public void addWorkingExperience(WorkingExperience workingExperience) {
-        this.workingExperienceRepository.save(workingExperience);
-    }
+	@Transactional
+	public void addWorkingExperience(WorkingExperience workingExperience) {
+		this.workingExperienceRepository.save(workingExperience);
+	}
 
-    public List<WorkingExperience> getWorkingExperiences() {
-        return workingExperienceRepository.findAll();
-    }
+	public List<WorkingExperience> getWorkingExperiences() {
+		return workingExperienceRepository.findAll();
+	}
 
-    @Transactional
-    public void updateWorkingExperience(Long workingExperienceId, LocalDate startedWork, LocalDate finishedWork, String jobDescription, String jobDescriptionIt, Long positionAtWorkId) {
-        Optional<WorkingExperience> workingExperienceOptional = this.workingExperienceRepository.findById(workingExperienceId);
-        if (workingExperienceOptional.isPresent()) {
-            WorkingExperience workingExperience = workingExperienceOptional.get();
-            if (startedWork != null) {
-                workingExperience.setStartedWork(startedWork);
-                this.workingExperienceRepository.save(workingExperience);
-            }
-            if (finishedWork != null) {
-                workingExperience.setFinishedWork(finishedWork);
-                this.workingExperienceRepository.save(workingExperience);
-            }
-            if (jobDescription != null) {
-                workingExperience.setJobDescription(jobDescription);
-                this.workingExperienceRepository.save(workingExperience);
-            }
-            if (jobDescriptionIt != null) {
-                workingExperience.setJobDescriptionIt(jobDescriptionIt);
-                this.workingExperienceRepository.save(workingExperience);
-            }
-            if (positionAtWorkId != null) {
-                PositionAtWork positionAtWork = positionAtWorkRepository.findById(positionAtWorkId)
-                        .orElseThrow(() -> new IllegalStateException("PositionAtWork with id " + positionAtWorkId + " does not exist"));
-                workingExperience.setPositionAtWork(positionAtWork);
-            }
-        } else {
-            throw new IllegalStateException("Working experience with id " + workingExperienceId + " doesn't exists");
-        }
-    }
+	@Transactional
+	public void updateWorkingExperience(Long workingExperienceId, LocalDate startedWork, LocalDate finishedWork,
+			String jobDescription, String jobDescriptionIt, Long positionAtWorkId) {
+		Optional<WorkingExperience> workingExperienceOptional = this.workingExperienceRepository
+				.findById(workingExperienceId);
+		if (workingExperienceOptional.isPresent()) {
+			WorkingExperience workingExperience = workingExperienceOptional.get();
+			if (startedWork != null) {
+				workingExperience.setStartedWork(startedWork);
+				this.workingExperienceRepository.save(workingExperience);
+			}
+			if (finishedWork != null) {
+				workingExperience.setFinishedWork(finishedWork);
+				this.workingExperienceRepository.save(workingExperience);
+			}
+			if (jobDescription != null) {
+				workingExperience.setJobDescription(jobDescription);
+				this.workingExperienceRepository.save(workingExperience);
+			}
+			if (jobDescriptionIt != null) {
+				workingExperience.setJobDescriptionIt(jobDescriptionIt);
+				this.workingExperienceRepository.save(workingExperience);
+			}
+			if (positionAtWorkId != null) {
+				PositionAtWork positionAtWork = positionAtWorkRepository.findById(positionAtWorkId)
+						.orElseThrow(() -> new IllegalStateException(
+								"PositionAtWork with id " + positionAtWorkId + " does not exist"));
+				workingExperience.setPositionAtWork(positionAtWork);
+			}
+		} else {
+			throw new IllegalStateException("Working experience with id " + workingExperienceId + " doesn't exists");
+		}
+
+	}
+
+	@Transactional
+	public void deleteWorkingExperience(Long workingExperienceId) {
+		this.workingExperienceRepository.deleteById(workingExperienceId);
+	}
 }
